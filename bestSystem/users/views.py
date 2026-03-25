44684230780dec19ff2c518ff6053fbd3e75f90f
ahmadhossam24 <70440432+ahmadhossam24@users.noreply.github.com>
2026-03-25 11:47:24 +0200

    django-filters error

--- bestSystem/users/views.py
+++ bestSystem/users/views.py
@@ -2,12 +2,14 @@ from rest_framework import viewsets, status, filters
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import permissions
 from django.db.models import Q
 from .models import User, Branch, Team, EditLog, UserRole
 from .serializers import (
     UserSerializer, BranchSerializer, TeamSerializer, EditLogSerializer
 )
 from .permissions import IsCompanyBoss, IsBranchAdmin, IsTeamAdmin, IsOwnerOrCreator, IsCreatorOrAdmin
+from datetime import timezone
 
 class UserViewSet(viewsets.ModelViewSet):
     """
